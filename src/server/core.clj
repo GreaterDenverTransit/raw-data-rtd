d605cc10f56129342c36928b53352314105ff82a
Emma Griffin <emma.audrey.g@gmail.com>
2025-02-08 18:54:12 -0700

    Cleanup

--- src/server/core.clj
+++ src/server/core.clj
@@ -7,21 +7,22 @@
   "Initialize server"
   [& _args]
   (let [port (parse-long (or (System/getenv "PORT") "8080"))]
-    (server/run-server #'routes/app-routes {:port port})
-    (println (str "Running webserver at http://127.0.0.1:" port "/"))))
+    (println (str "Running webserver at http://127.0.0.1:" port "/"))
+    (server/run-server #'routes/app-routes {:port port})))
 
 (comment
   (require '[org.httpkit.client :as client])
 
   (start)
 
+  #_:clj-kondo/ignore
   @(client/get "http://127.0.0.1:8080/health")
   ;; =>
-  {:opts   {:method :get, :url "http://127.0.0.1:8080/health"},
-   :body   "",
+  {:opts   {:method :get, :url "http://127.0.0.1:8080/health"}
+   :body   ""
    :headers
-   {:content-length "0",
-    :content-type   "text/json",
-    :date           "Sun, 2 Feb 2025 04:56:05 GMT",
-    :server         "http-kit"},
+   {:content-length "0"
+    :content-type   "text/json"
+    :date           "Sun, 2 Feb 2025 04:56:05 GMT"
+    :server         "http-kit"}
    :status 200})
