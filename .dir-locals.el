56fb9957a7137d43fc8c0da18b62af9baf3d5d76
Emma Griffin <53583563+E-A-Griffin@users.noreply.github.com>
2025-02-15 13:06:14 -0700

    Make #p work (#5)

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,4 @@
+;; Refer to https://github.com/rads/preload for user-level config expected with
+;; CIDER on this project
 ((nil
-  (cider-clojure-cli-global-options . "-A:test")))
+  (cider-clojure-cli-global-options . "-A:preload:test:nrepl")))
